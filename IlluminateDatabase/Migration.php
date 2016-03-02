7fc8e6cc7959f4d5e36883d164c797ab29f5dd79
Cláudio Silva <claudio.silva@impactwave.com>
2016-03-02 12:16:38 +0000

    Use DbUtil.

--- IlluminateDatabase/Migration.php
+++ IlluminateDatabase/Migration.php
@@ -1,10 +1,10 @@
 <?php
 namespace Selenia\Plugins\IlluminateDatabase;
 
-use ErrorException;
 use Illuminate\Database\Query\Builder;
 use Phinx\Migration\AbstractMigration;
 use PhpKit\Connection;
+use Selenia\Database\Lib\DbUtil;
 
 /**
  * Integrates Illuminate Database with Phinx migrations to allow the developer to create migrations on Selenia using
@@ -14,46 +14,6 @@ class Migration extends AbstractMigration
 {
   /** @var DatabaseAPI */
   protected $db;
-
-  /**
-   * Parses CSV-formatted data from a string and returns it as an array of arrays.
-   *
-   * @param array|string $columns An array of column names, or a string of comma-delimited column names.
-   * @param string       $csv     The CSV data.
-   * @return array The loaded data.
-   */
-  static function loadCSV ($columns, $csv)
-  {
-    if (is_string ($columns)) $columns = explode (',', $columns);
-// Use an I/O stream instead of an actual file.
-    $handle = fopen ('php://temp/myCSV', 'w+b');
-
-// Write all the data to it
-    fwrite ($handle, $csv);
-
-// Rewind for reading
-    rewind ($handle);
-
-// use fgetcsv which tends to work better than str_getcsv in some cases
-    $data = [];
-    $i    = 0;
-    try {
-      while ($row = fgetcsv ($handle, null, ',', "'")) {
-        ++$i;
-        $data[] = array_combine ($columns, $row);
-      }
-      fclose ($handle);
-    }
-    catch (ErrorException $e) {
-      echo "\nInvalid row #$i\n\nColumns:\n";
-      var_export ($columns);
-      echo "\n\nRow:\n";
-      var_export ($row);
-      echo "\n";
-      exit (1);
-    }
-    return $data;
-  }
 
   /**
    * Imports data from a CSV-formatted string into a database table.
@@ -67,7 +27,7 @@ class Migration extends AbstractMigration
    */
   protected function importCSV ($table, $columns, $csv, $connection = null)
   {
-    $data  = self::loadCSV ($columns, $csv);
+    $data  = DbUtil::loadCSV ($columns, $csv);
     $table = $this->db->table ($table, $connection);
     $table->insert ($data);
     return $table;
