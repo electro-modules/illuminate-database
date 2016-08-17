950a77aaa04f1a7baea288cd0220a6ec915369a1
Cláudio Silva <claudio.silva@impactwave.com>
2016-08-17 20:43:40 +0100

    Implementing Migrations.

--- IlluminateDatabase/Services/Migrations.php
+++ IlluminateDatabase/Services/Migrations.php
@@ -2,10 +2,14 @@
 namespace Electro\Plugins\IlluminateDatabase\Services;
 
 use Electro\Core\Assembly\ModuleInfo;
+use Electro\Core\Assembly\Services\ModulesRegistry;
 use Electro\Interfaces\MigrationsInterface;
+use Electro\Interop\MigrationStruct as Migration;
 use Electro\Plugins\IlluminateDatabase\DatabaseAPI;
 use Electro\Plugins\IlluminateDatabase\Migrations\Config\MigrationsSettings;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Schema\Blueprint;
+use PhpKit\Flow\FilesystemFlow;
 
 class Migrations implements MigrationsInterface
 {
@@ -15,60 +19,110 @@ class Migrations implements MigrationsInterface
    */
   private $databaseAPI;
   /**
+   * @var string
+   */
+  private $migrationsPath;
+  /**
+   * @var ModuleInfo
+   */
+  private $module;
+  /**
+   * @var ModulesRegistry
+   */
+  private $registry;
+  /**
+   * @var string
+   */
+  private $seedsPath;
+  /**
    * @var MigrationsSettings
    */
   private $settings;
 
-  public function __construct (DatabaseAPI $databaseAPI, MigrationsSettings $settings) {
+
+  public function __construct (DatabaseAPI $databaseAPI, MigrationsSettings $settings, ModulesRegistry $registry)
+  {
     $this->databaseAPI = $databaseAPI;
-    $this->settings = $settings;
+    $this->settings    = $settings;
+    $this->registry    = $registry;
   }
 
-  private function ensureMigrationsTableExists ()
+  function migrate ($target = null)
   {
-    $schema = $this->databaseAPI->schema();
-    if (!$schema->hasTable(self::MIGRATIONS_TABLE))
-      $schema->create(self::MIGRATIONS_TABLE, function (Blueprint $table) {
-        $table->increments('id');
-        $table->dateTime('');
+    $this->assertModuleIsSet ();
+
+  }
+
+  function module ($moduleName)
+  {
+    $this->module         = $this->registry->getModule ($moduleName);
+    $this->migrationsPath = "{$this->module->path}/{$this->settings->migrationsPath ()}";
+    $this->seedsPath      = "{$this->module->path}/{$this->settings->seedsPath ()}";
+    $this->createMigrationsTableIfRequired ();
+    return $this;
+  }
+
+  function reset ()
+  {
+    $this->assertModuleIsSet ();
+
+  }
+
+  function rollback ($target = null, $date = null)
+  {
+    $this->assertModuleIsSet ();
+
+  }
+
+  function seed ($seeder = null)
+  {
+    $this->assertModuleIsSet ();
+
+  }
+
+  function status ($onlyPending = false)
+  {
+    $this->assertModuleIsSet ();
+    $all = $this->getTable ()->orderBy (Migration::date)->get ();
+    return $onlyPending ? array_findAll ($all, Migration::status, Migration::DOWN) : $all;
+  }
+
+  private function assertModuleIsSet ()
+  {
+    if (!$this->module)
+      throw new \RuntimeException("Target module is not set");
+  }
+
+  private function createMigrationsTableIfRequired ()
+  {
+    $schema = $this->databaseAPI->schema ();
+    if (!$schema->hasTable (self::MIGRATIONS_TABLE))
+      $schema->create (self::MIGRATIONS_TABLE, function (Blueprint $table) {
+        $table->increments (Migration::id);
+        $table->string (Migration::date, 14);
+        $table->string (Migration::module, 64);
+        $table->string (Migration::name, 128);
+        $table->string (Migration::status, 4);
+        $table->string (Migration::reverse, 1024);
+        $table->index (Migration::date);
+        $table->index (Migration::module);
       });
   }
 
-  function migrate ($moduleName, $target = null)
+  /**
+   * @return Builder
+   */
+  private function getTable ()
   {
-
-  }
-
-  function reset ($moduleName)
-  {
-    // TODO: Implement reset() method.
-  }
-
-  function rollback ($moduleName, $target = null, $date = null)
-  {
-    // TODO: Implement rollback() method.
-  }
-
-  function seed ($moduleName, $seeder = null)
-  {
-    // TODO: Implement seed() method.
-  }
-
-  function status ($moduleName, $onlyPending = false)
-  {
-    // TODO: Implement status() method.
+    return $this->databaseAPI->table (self::MIGRATIONS_TABLE)->where (Migration::module, $this->module->name);
   }
 
   /**
-   * @param string|ModuleInfo $module
-   * @return bool
+   * @return string[]
    */
-  private function moduleHasMigrations ($module)
+  private function getAllFiles ()
   {
-    if (is_string ($module))
-      $module = $this->registry->getModule ($module);
-    $path = "$module->path/" . $this->migrationsSettings->migrationsPath ();
-    return fileExists ($path);
+    return FilesystemFlow::glob("{$this->migrationsPath}/*.php")->keys()->sort()->all ();
   }
 
 }
