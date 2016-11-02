20d1ec05bfebdb2551f02d759e332072da90462a
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-02 03:56:46 +0000

    Finished upgrading.

--- IlluminateDatabase/Config/IlluminateDatabaseModule.php
+++ IlluminateDatabase/Config/IlluminateDatabaseModule.php
@@ -1,8 +1,9 @@
 <?php
 namespace Electro\Plugins\IlluminateDatabase\Config;
 
+use Electro\Core\Assembly\ModuleInfo;
 use Electro\Core\Assembly\Services\Bootstrapper;
-use Electro\Core\Assembly\Services\ModuleServices;
+use Electro\Core\ConsoleApplication\Config\ConsoleSettings;
 use Electro\Interfaces\DI\InjectorInterface;
 use Electro\Interfaces\Migrations\MigrationsInterface;
 use Electro\Interfaces\ModelControllerInterface;
@@ -12,26 +13,32 @@ use Electro\Plugins\IlluminateDatabase\DatabaseAPI;
 use Electro\Plugins\IlluminateDatabase\Services\Migrations;
 use Electro\Plugins\IlluminateDatabase\Services\ModelController;
 use Illuminate\Events\Dispatcher;
+use const Electro\Core\Assembly\Services\CONFIGURE;
+use const Electro\Core\Assembly\Services\REGISTER_SERVICES;
 
 class IlluminateDatabaseModule implements ModuleInterface
 {
-  static function boot (Bootstrapper $boot)
+  static function bootUp (Bootstrapper $bootstrapper, ModuleInfo $moduleInfo)
   {
-    $boot->on (Bootstrapper::EVENT_BOOT, function (InjectorInterface $injector, ModuleServices $module) {
-      $injector
-        ->share (DatabaseAPI::class)
-        ->prepare (DatabaseAPI::class, function (DatabaseAPI $db) {
-          $db->manager->setAsGlobal ();
-          $db->manager->setEventDispatcher (new Dispatcher($db->manager->getContainer ()));
-          $db->manager->bootEloquent ();
-        })
-        ->alias (ModelControllerInterface::class, ModelController::class)
-        ->share (ModelController::class)
-        ->share (MigrationsSettings::class)
-        ->alias (MigrationsInterface::class, Migrations::class);
-
-      $module->registerTasksFromClass (MigrationCommands::class);
-    });
+    $bootstrapper
+      //
+      ->on (REGISTER_SERVICES, function (InjectorInterface $injector) {
+        $injector
+          ->share (DatabaseAPI::class)
+          ->prepare (DatabaseAPI::class, function (DatabaseAPI $db) {
+            $db->manager->setAsGlobal ();
+            $db->manager->setEventDispatcher (new Dispatcher($db->manager->getContainer ()));
+            $db->manager->bootEloquent ();
+          })
+          ->alias (ModelControllerInterface::class, ModelController::class)
+          ->share (ModelController::class)
+          ->share (MigrationsSettings::class)
+          ->alias (MigrationsInterface::class, Migrations::class);
+      })
+      //
+      ->on (CONFIGURE, function (ConsoleSettings $consoleSettings) {
+        $consoleSettings->registerTasksFromClass (MigrationCommands::class);
+      });
   }
 
 }
