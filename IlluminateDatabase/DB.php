419a38af4d149f7cc75eb7b238bd23e0411a3469
Cláudio Silva <claudio.silva@impactwave.com>
2017-02-20 12:33:46 +0000

    New DB::connection() shortcut for compat.

--- IlluminateDatabase/DB.php
+++ IlluminateDatabase/DB.php
@@ -1,7 +1,13 @@
 <?php
+
 namespace Electro\Plugins\IlluminateDatabase;
 
 /**
+ * Please DO NOT USE this class, except on database migrations.
+ *
+ * <p>Its purpose is to ease the conversion of existing Laravel migrations to Electro migrations.
+ * <p>Use the {@see DatabaseAPI} injectable class instead.
+ *
  * @method static \Illuminate\Database\Query\Builder table(string $table) Begin a fluent query against a database table.
  */
 class DB
@@ -12,6 +18,15 @@ class DB
   public static function __callStatic ($method, $args)
   {
     return self::$api->connection ()->$method (...$args);
+  }
+
+  /**
+   * @param string $name The connection name.
+   * @return \Illuminate\Database\Connection
+   */
+  public static function connection ($name)
+  {
+    return self::$api->connection ($name);
   }
 
   public static function setInstance (DatabaseAPI $api)
