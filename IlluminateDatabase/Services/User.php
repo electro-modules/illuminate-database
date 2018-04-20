3e3c0f3b6e21fac0f6ed17c0f72909f8621378cc
Tiago Elias <tiagoelias1999@gmail.com>
2018-04-20 15:13:29 +0100

    Remoção do métodos xxxField()

--- IlluminateDatabase/Services/User.php
+++ IlluminateDatabase/Services/User.php
@@ -8,35 +8,39 @@ use Electro\Plugins\IlluminateDatabase\BaseModel;
 
 class User extends BaseModel implements UserInterface
 {
+  const CREATED_AT = 'registrationDate';
+  const UPDATED_AT = 'updatedAt';
   public $gender     = '$USER_THE';
   public $plural     = '$USERS';
   public $singular   = '$USER';
   public $timestamps = true;
-
   protected $casts = [
     'active'  => 'boolean',
     'enabled' => 'boolean',
   ];
   protected $dates = ['registrationDate', 'lastLogin'];
 
-  const CREATED_AT = 'createdAt';
-  const UPDATED_AT = 'updatedAt';
-
   public static function boot ()
   {
     parent::boot ();
 
     static::creating (function ($model) {
-      $model->createdAt = $model->freshTimestamp ();
-      $model->updatedAt = $model->freshTimestamp ();
+      $model->registrationDate = $model->freshTimestamp ();
+      $model->updatedAt        = $model->freshTimestamp ();
     });
   }
 
-  function activeField ($set = null)
+  public function findByEmail ($email)
   {
-    if (isset($set))
-      $this->active = $set;
-    return $this->active;
+
+    /** @var static $user */
+    $user = static::where ('email', $email)->first ();
+    if ($user) {
+      $this->forceFill ($user->getAttributes ())->syncOriginal ();
+      $this->exists = true;
+      return true;
+    }
+    return false;
   }
 
   public function findById ($id)
@@ -63,19 +67,6 @@ class User extends BaseModel implements UserInterface
     return false;
   }
 
-  public function findByEmail ($email)
-  {
-
-    /** @var static $user */
-    $user = static::where ('email', $email)->first ();
-    if ($user) {
-      $this->forceFill ($user->getAttributes ())->syncOriginal ();
-      $this->exists = true;
-      return true;
-    }
-    return false;
-  }
-
   public function findByToken ($token)
   {
     /** @var static $user */
@@ -88,20 +79,21 @@ class User extends BaseModel implements UserInterface
     return false;
   }
 
-  public function getRecord ()
+  public function getFields ()
   {
     return [
-      'active'           => $this->activeField (),
-      'id'               => $this->idField (),
-      'lastLogin'        => $this->lastLoginField (),
-      'realName'         => $this->realNameField (),
-      'registrationDate' => $this->registrationDateField (),
-      'role'             => $this->roleField (),
-      'token'            => $this->tokenField (),
-      'username'         => $this->usernameField (),
-      'email'            => $this->emailField (),
-      'password'         => $this->passwordField (),
-      'enabled'          => $this->enabledField (),
+      'active'           => $this->active,
+      'id'               => $this->id,
+      'lastLogin'        => $this->lastLogin,
+      'realName'         => $this->realName,
+      'registrationDate' => $this->registrationDate,
+      'updatedAt'        => $this->updatedAt,
+      'role'             => $this->role,
+      'token'            => $this->token,
+      'username'         => $this->username,
+      'email'            => $this->email,
+      'password'         => '',
+      'enabled'          => $this->enabled,
     ];
   }
 
@@ -110,18 +102,19 @@ class User extends BaseModel implements UserInterface
     return $this->newQuery ()->where ('role', '<=', $this->role)->orderBy ('username')->get ()->all ();
   }
 
-  function idField ($set = null)
+  function mergeFields ($data)
   {
-    if (isset($set))
-      $this->id = $set;
-    return $this->id;
-  }
+    if (exists (get ($data, 'password'))) {
+      $this->password = password_hash (get ($data, 'password'), PASSWORD_BCRYPT);
+    }
 
-  function lastLoginField ($set = null)
-  {
-    if (isset($set))
-      $this->lastLogin = $set;
-    return $this->lastLogin;
+    $this->active   = get ($data, 'active', 0);
+    $this->enabled  = get ($data, 'enabled', 1);
+    $this->realName = get ($data, 'realName');
+    $this->email    = get ($data, 'email');
+    $this->token    = get ($data, 'token');
+    $this->username = get ($data, 'username');
+    $this->role     = get ($data, 'role', UserInterface::USER_ROLE_STANDARD);
   }
 
   function onLogin ()
@@ -130,100 +123,21 @@ class User extends BaseModel implements UserInterface
     $this->submit ();
   }
 
-  function passwordField ($set = null)
-  {
-    if (isset($set))
-      $this->password = password_hash ($set, PASSWORD_BCRYPT);
-    return $this->password;
-  }
-
-  function realNameField ($set = null)
-  {
-    if (isset($set))
-      return $this->realName = $set;
-    return $this->realName ?: ucfirst ($this->usernameField ());
-  }
-
-  function registrationDateField ($set = null)
-  {
-    if (isset($set))
-      $this->created_at = $set;
-    return $this->created_at;
-  }
-
-  function roleField ($set = null)
-  {
-    if (isset($set))
-      $this->role = $set;
-    return $this->role;
-  }
-
-  function tokenField ($set = null)
-  {
-    if (isset($set))
-      $this->token = $set;
-    return $this->token;
-  }
-
-  function usernameField ($set = null)
-  {
-    if (isset($set))
-      $this->username = $set;
-    return $this->username;
-  }
-
-  function enabledField ($set = null)
-  {
-    if (isset($set))
-      $this->enabled = $set;
-    return $this->enabled;
-  }
-
-  function verifyPassword ($password)
-  {
-    if ($password == $this->password) {
-      // Migrate plain text password to hashed version.
-      $this->passwordField ($password);
-      $this->submit ();
-      return true;
-    }
-    return password_verify ($password, $this->password);
-  }
-
-  function emailField ($set = null)
-  {
-    if (isset($set))
-      $this->email = $set;
-    return $this->email;
-  }
-
   function remove ()
   {
     $this->delete ();
   }
 
-  function setRecord ($data)
-  {
-    $newPassword = password_hash (get ($data, 'password'), PASSWORD_BCRYPT);
-
-    $username = get ($data, 'username');
-    $now      = Carbon::now ();
-    $email    = get ($data, 'email');
-    $realName = get ($data, 'realName');
-    $token    = get ($data, 'token');
-
-    $this->email            = $email;
-    $this->username         = $username;
-    $this->realName         = $realName;
-    $this->token            = $token;
-    $this->password         = $newPassword;
-    $this->registrationDate = $now;
-    $this->role             = UserInterface::USER_ROLE_STANDARD;
-    $this->active           = 0;
-  }
-
   function submit ()
   {
     $this->save ();
+  }
+
+  function verifyPassword ($password)
+  {
+    if ($password == $this->password) {
+      return true;
+    }
+    return password_verify ($password, $this->password);
   }
 }
